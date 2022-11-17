93f40d670d3c8b2489af5c6e6b920cdfc6925487
div31 <divyasuri343@gmail.com>
2022-11-17 02:17:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Homepage from "./components/homepage/Homepage"
+import Login from "./components/login/Login"
+import About from './components/homepage/About';
+import Jobs from './components/homepage/Jobs';
+import Contact from './components/homepage/Contact';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
 
+} from "react-router-dom";
+import { Fragment, useState } from 'react';
 function App() {
+  const [user, setLoginUser] = useState({
+
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/homepage" element={<Homepage/>}></Route>
+          <Route exact path="/homepage/about" element={<About/>}></Route>
+          <Route exact path="/homepage/jobs" element={<Jobs/>}></Route>
+          <Route exact path="/homepage/contact" element={<Contact/>}></Route>
+          <Route exact path="/" element={<Login setLoginUser={setLoginUser} />}></Route>
+        </Routes>
+      </Router>
+
     </div>
   );
 }
