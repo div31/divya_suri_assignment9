e197547e24eb02b9d9ab388cce452a4a32e954ef
div31 <divyasuri343@gmail.com>
2022-11-20 19:27:07 -0500

    login form styling changes

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,34 +1,35 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import {useNavigate, Navigate} from "react-router-dom"
-const Login = ({setLoginUser}) => {
-const navigate = useNavigate()
-    const [user,setUser] = useState({
-        name:"",
+import { useNavigate, Navigate } from "react-router-dom";
+import './Login.css';
+const Login = ({ setLoginUser }) => {
+    const navigate = useNavigate()
+    const [user, setUser] = useState({
+        name: "",
         password: ""
     })
     const [signedIn, setSignedIn] = useState(false)
-    const handleChange = e =>{
-    const {name,value} = e.target
-    setUser({
-    ...user,//spread operator 
-    [name]:value
-    })
+    const handleChange = e => {
+        const { name, value } = e.target
+        setUser({
+            ...user,//spread operator 
+            [name]: value
+        })
     }
 
-    const login =(e)=>{
+    const login = (e) => {
         e.preventDefault();
         console.log(user)
-        axios.post("http://localhost:3000/Login",user)
-        .then(res=>{
-            alert(res.data.message)
-            if(res.data.message === "login sucess"){
-                navigate('/homepage')
-            }
-            console.log(res.data.user)
-            setLoginUser(res.data.user)
-            setSignedIn(!signedIn)
-        })
+        axios.post("http://localhost:3000/Login", user)
+            .then(res => {
+                alert(res.data.message)
+                if (res.data.message === "login sucess") {
+                    navigate('/homepage')
+                }
+                console.log(res.data.user)
+                setLoginUser(res.data.user)
+                setSignedIn(!signedIn)
+            })
 
     }
     // if(signedIn){
@@ -36,31 +37,27 @@ const navigate = useNavigate()
     // }
     return (
         <>
-<div class="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
-    <div class="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl dark:text-white">
-        Login To Your Account
-    </div>
-    <div class="mt-8">
-        <form autoComplete="off">
-            <div class="flex flex-col mb-2">
-                <div class="flex relative ">
-                    <input type="text" id="sign-in-email" class=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="email" value={user.email}  onChange={handleChange} placeholder="Your email"/>
-                    </div>
-                </div>
-                <div class="flex flex-col mb-6">
-                    <div class="flex relative ">
-                        <input type="password" id="sign-in-password" class=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="password" value={user.password}  onChange={handleChange} placeholder="Your password"/>
+
+            <div class="global-container">
+                <div class="card login-form">
+                    <div class="card-body">
+                        <h3 class="card-title text-center">Login</h3>
+                        <div class="card-text">
+                            <form>
+                                <div class="form-group">
+                                    <label for="sign-in-email">Email address</label>
+                                    <input type="email" class="form-control form-control-sm" id="sign-in-email" aria-describedby="emailHelp" value={user.email} onChange={handleChange} name="email" />
+                                </div>
+                                <div class="form-group">
+                                    <label for="sign-in-password">Password</label>
+                                    <input type="password" class="form-control form-control-sm" id="sign-in-password" value={user.password} onChange={handleChange} name="password" />
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-block" onClick={login}>Sign in</button>
+                            </form>
                         </div>
                     </div>
-                    <div class="flex w-full">
-                        <button type="submit" class="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-black w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg  " onClick={login}>
-                            Login
-                        </button>
-                    </div>
-                </form>
+                </div>
             </div>
-        </div>
-
         </>
     )
 }
